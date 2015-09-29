a8a6f7048fccd9d9ab8d96fcb6bd31966c715c79
Aleksi Luomala <aleksiluo1@AOTA1-1LDML-09D.kamit.fi>
2015-09-29 13:22:33 +0300

    KC stuffia

---
+++ ConsoleApplication1/KC in da house/KC in da house/Stack.h
@@ -0,0 +1,59 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include <vector>
+#include <iostream>
+#include <string>
+
+template <class T> 
+class Stack
+{
+public:
+	Stack( int size);
+	~Stack();
+	void add(const T &i);
+	T pop();
+
+
+private:
+	int max;
+	int top;
+	T *stack
+	int[] *size;
+	//int vec[];
+}; 
+
+template<class T>
+Stack(int s)
+{
+	T *size = new T[s];
+}
+
+
+template <class T>
+add(const T &i)
+{
+	
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
